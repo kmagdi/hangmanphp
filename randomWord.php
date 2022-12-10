f6467eada3ef4d2c236f5320f70d5229e085899d
KMagdi <46417302+kmagdi@users.noreply.github.com>
2022-12-10 20:17:15 +0100

    db.php

--- randomWord.php
+++ randomWord.php
@@ -9,13 +9,18 @@ header('Access-Control-Allow-Credentials: true');
 header('Access-Control-Allow-Headers: Authorization, Content-Type, x-xsrf-token, x_csrftoken, Cache-Control, X-Requested-With');
 
 //require_once 'db_cloud.php';
-require_once 'db_localhost.php';
+require_once 'db.php';
 
-echo "ellenőrzés:".$host;
+//echo "ellenőrzés:".$host;
+try{
+    $sql="SELECT * FROM words ORDER BY RAND() LIMIT 1";
+    $stmt=$db->query($sql);
+    echo json_encode($stmt->fetchAll());
+}catch(PDOException $e){
+    echo json_encode(["error"=>$e->getMessage()]);
+}
 
-$sql="SELECT * FROM words ORDER BY RAND() LIMIT 1";
-$stmt=$db->query($sql);
 
-echo json_encode($stmt->fetchAll());
+
 ?>
 
